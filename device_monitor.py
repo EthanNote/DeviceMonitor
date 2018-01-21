ec8a8adf9a867943cf46d487223533c3262299ad
EthanNote <hanyishen32@foxmail.com>
2018-01-21 14:46:04 +0800

    Update device_monitor.py

--- device_monitor.py
+++ device_monitor.py
@@ -2,39 +2,10 @@ import htmlPy
 import os
 
 device_list=[
-    ('Device A', [
-        ('IP','192.168.1.1'),
-        ('position', 'F1')
-    ]),
-    ('Device B', [
-        ('IP','192.168.1.2'),
-        ('position', 'F2')
-    ]),
-    ('Device C', [
-        ('IP','192.168.1.3'),
-        ('position', 'F3')
-    ]),
-    ('Device D', [
-        ('IP','192.168.1.4'),
-        ('position', 'F4')
-    ]),
-    ('Device E', [
-        ('IP','192.168.1.5'),
-        ('position', 'F5')
-    ]),
-    ('Device F', [
-        ('IP','192.168.1.6'),
-        ('position', 'F6')
-    ]),
-    ('Device G', [
-        ('IP','192.168.1.7'),
-        ('position', 'F7')
-    ]),
-    ('Device H', [
-        ('IP','192.168.1.8'),
-        ('position', 'F8')
-    ]),
-
+    ('Device A', [        ('IP','192.168.1.1'),        ('position', 'F1')    ]),
+    ('Device B', [        ('IP','192.168.1.2'),        ('position', 'F2')    ]),
+    ('Device C', [        ('IP','192.168.1.3'),        ('position', 'F3')    ]),
+    ('Device D', [        ('IP','192.168.1.4'),        ('position', 'F4')    ])
 ]
 
 class JavascriptBuildinAPI(htmlPy.Object):
